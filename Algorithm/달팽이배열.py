58785e481272341e2f92f2dea3f4ea14fe0db209
sururuu <60089873+sururuu@users.noreply.github.com>
2020-03-12 19:14:30 +0900

    Add files via upload

---
+++ Algorithm/달팽이배열.py
@@ -0,0 +1,38 @@
+t=int(input())
+for tc in range(1,t+1):
+    n=int(input())
+    arr=[[0]*n for i in range(n)]
+    cnt=1
+    row_start=0
+    col_start=0
+    row_end=n-1
+    col_end=n-1
+    while row_start<=row_end and col_start<=col_end:
+        #왼쪽 -> 오른쪽
+        for i in range(col_start,col_end+1):
+            arr[row_start][i]=cnt
+            cnt+=1
+        row_start+=1
+
+        #위 -> 아래
+        for i in range(row_start,row_end+1):
+            arr[i][col_end]=cnt
+            cnt+=1
+        col_end-=1
+
+        #오른쪽 -> 왼쪽
+        for i in range(col_end,col_start-1,-1):
+            arr[row_end][i]=cnt
+            cnt+=1
+        row_end-=1
+
+        #아래 -> 위
+        for i in range(row_end,row_start-1,-1):
+            arr[i][col_start]=cnt
+            cnt+=1
+        col_start+=1
+    print("#{}".format(tc))
+    for i in range(n):
+        for j in range(n):
+            print(arr[i][j],end=' ')
+        print()
