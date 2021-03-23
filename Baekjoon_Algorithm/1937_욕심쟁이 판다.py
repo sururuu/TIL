f7efb731eba9da96fe504f8ed665236c0bd81fc1
sururuu <60089873+sururuu@users.noreply.github.com>
2021-03-23 23:42:16 +0900

    1937

---
+++ Baekjoon_Algorithm/1937_욕심쟁이 판다.py
@@ -0,0 +1,22 @@
+import sys
+sys.setrecursionlimit(10**6)
+dx = [0, -1, 0, 1]
+dy = [-1, 0, 1, 0]
+def dfs(i,j):
+    if res[i][j]:
+        return res[i][j]
+    res[i][j] = 1
+    for k in range(4):
+        nx,ny = i+dx[k], j+dy[k]
+        if 0<=nx<n and 0<=ny<n:
+            if forest[i][j] < forest[nx][ny]:
+                res[i][j] = max(res[i][j],dfs(nx,ny)+1)
+    return res[i][j]
+n = int(input())
+forest = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+res = [[0]*n for _ in range(n)]
+ans = 0
+for i in range(n):
+    for j in range(n):
+        ans = max(ans,dfs(i,j))
+print(ans)
