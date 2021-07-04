d5b5d1589114bee2e0569beba701d46fb7b9b516
sururuu <60089873+sururuu@users.noreply.github.com>
2021-07-04 23:47:35 +0900

    16194

---
+++ Baekjoon_Algorithm/16194_카드 구매하기2.py
@@ -0,0 +1,8 @@
+n = int(input())
+card = [0] + list(map(int,input().split()))
+dp = [0] * (n+1)
+for i in range(1,n+1):
+    dp[i] = card[i]
+    for j in range(1,i+1):
+        dp[i] = min(dp[i], dp[i-j]+card[j])
+print(dp[-1])
