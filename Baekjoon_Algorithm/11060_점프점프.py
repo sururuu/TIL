504c98666efedcd5d7d84d4437d2e8ed705bae8e
sururuu <60089873+sururuu@users.noreply.github.com>
2021-07-10 23:57:25 +0900

    11060

---
+++ Baekjoon_Algorithm/11060_점프점프.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(int,input().split()))
+dp = [n+1]*n
+dp[0] = 0
+for i in range(n):
+    for j in range(1, arr[i]+1):
+        if i + j >= n:
+            break
+        dp[i+j] = min(dp[i+j], dp[i]+1)
+if dp[-1]:
+    print(dp[-1])
+else:
+    print(-1)
