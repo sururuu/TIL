515a0b5013b4d0f0f1600fe95cd3977b9fa806e6
sururuu <60089873+sururuu@users.noreply.github.com>
2021-09-19 00:17:24 +0900

    Update 2531_회전초밥.py

--- Baekjoon_Algorithm/2531_회전초밥.py
+++ Baekjoon_Algorithm/2531_회전초밥.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 def sol1():
     res = 0
     for i in range(n):
@@ -10,8 +11,31 @@ def sol1():
             break
         res = max(res, cnt)
     return res
+def sol2():
+    dict = defaultdict(int)
+    dict[c] = 1
+    for i in range(k):
+        dict[sushi[i]] += 1
+    cnt = len(dict)
+    ans = cnt
+    for i in range(k, len(sushi)):
+        prev = sushi[i-k]
+        temp = sushi[i]
+        if prev == temp:
+            continue
+        else:
+            dict[prev] -= 1
+            dict[temp] += 1
+            if dict[prev] == 0:
+                cnt -= 1
+            if dict[temp] == 1:
+                cnt += 1
+        ans = max(ans, cnt)
+        if ans == k+1:
+            break
+    return ans
 n, d, k, c = map(int, input().split())
 sushi = [int(input()) for _ in range(n)]
 plus = sushi[:k-1]
 sushi += plus
-print(sol1())
+print(sol2())
