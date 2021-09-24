4a2c9278d7087fb9fa2d830c25fcb7539f856e8d
sururuu <60089873+sururuu@users.noreply.github.com>
2021-09-24 23:55:52 +0900

    1780

---
+++ Baekjoon_Algorithm/1780_종이의 개수.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+cnt = [0, 0, 0]
+def sol(x,y,n):
+    visit = arr[x][y]
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if arr[i][j] != visit:
+                for k in range(3):
+                    for l in range(3):
+                        sol(x+k*n//3, y+l*n//3, n//3)
+                return
+    if visit == -1:
+        cnt[0] += 1
+    elif visit == 0:
+        cnt[1] += 1
+    else:
+        cnt[2] += 1
+sol(0,0,n)
+for i in range(3):
+    print(cnt[i])
