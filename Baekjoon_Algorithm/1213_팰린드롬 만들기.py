a98fc0e56e9211bed8b904df629225f144c0d658
sururuu <60089873+sururuu@users.noreply.github.com>
2021-10-02 23:33:12 +0900

    1213

---
+++ Baekjoon_Algorithm/1213_팰린드롬 만들기.py
@@ -0,0 +1,29 @@
+words = list(input())
+words.sort()
+alpha = {i: 0 for i in range(65, 91)}
+for word in words:
+    alpha[ord(word)] += 1
+res = ''
+other = ''
+flag = True
+for target in alpha:
+    idx = alpha[target]
+    if idx > 0:
+        if idx % 2 == 0:
+            r = idx // 2
+            res += chr(target) * r
+            alpha[target] = 0
+        else:
+            if not flag:
+                res = False
+                break
+            flag = False
+            r, q = divmod(idx, 2)
+            res += chr(target) * r
+            other += chr(target)
+            alpha[target] = 0
+if not res:
+    res = "I'm Sorry Hansoo"
+else:
+    res = res + other + res[::-1]
+print(res)
