9dbc200f348757d1b6a84082944a41becdcdc121
sururuu <60089873+sururuu@users.noreply.github.com>
2021-10-09 22:45:50 +0900

    1793

---
+++ Baekjoon_Algorithm/1793_타일링.py
@@ -0,0 +1,14 @@
+def sol(n):
+    if n == 0 or n == 1:
+        return 1
+    dp = [0] * (n+1)
+    dp[:2] = [1, 1]
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + 2 * dp[i-2]
+    return dp[n]
+while True:
+    try:
+        n = int(input())
+        print(sol(n))
+    except:
+        break
